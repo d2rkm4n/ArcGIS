a032afcf3a2803b8ee97abff9db81c8b91cf75ca
Luis Vargas <darkman87@gmail.com>
2023-06-20 17:38:53 -0600

    Create check_ha_data_store_svr_dist.py
    Cuando se tiene un ambiente en alta disponibilidad (HA), implica tener dos servidores de ArcGIS Data Store (Relacional) instalados y configurados en el ambiente; teniendo un servidor primario y secundario. 
    
    Por medio del REST del ArcGIS GIS Server (Hosted), se puede ingresar y validar cual de los dos servidores es el primario y cual es el secundario, así permite identificarlos y una vez que se sabe se puede por ejemplo aplicar por ejemplo parches en el nodo secundario sin afectar el primario.
    
    Este script en python utiliza el API de ArcGIS creado por ESRI, para que que se pueda ingresar como parametro url del portal, el nombre de un usuario y la contraseña (un usuario administrador); con esto el script hace la conexión al REST de ArcGIS Server, validar cuales son los servidores que forman parte del ambiente y retona como respuesta el nombre de cada uno así como si es el servidor primario, o bien el secundario.
    
    Output
    
    Machine name: SERVERDS1.DOMAIN.LOCAL
    Role: PRIMARY
    
    Machine name: SERVERDS2.DOMAIN.LOCAL
    Role: STANDBY

---
+++ check_ha_data_store_svr_dist.py
@@ -0,0 +1,46 @@
+import requests
+from getpass import getpass
+
+# Prompts the user to enter the portal URL, server URL, username and password
+age_dns = input('Enter AGE DNS: ')
+portal_url = f'https://{age_dns}/portal' # Only the name of the server where the WA is located is required, e.g. server.domain.local
+server_url = f'https://{age_dns}/server' # Only the name of the server where the WA is located is required, e.g. server.domain.local
+username = input('Enter username: ') # Portal Admin user
+password = getpass('Enter password: ')
+
+# Generate the access token
+token_url = f'{portal_url}/sharing/rest/generateToken'
+token_params = {
+    'username': username,
+    'password': password,
+    'client': 'referer',
+    'referer': portal_url,
+    'expiration': 60,
+    'f': 'json'
+}
+
+response = requests.post(token_url, data=token_params)
+response.raise_for_status()
+token = response.json()['token']
+
+# Specify the name of the ArcGIS Data Store relational
+datastore_name = input('Enter the name of the ArcGIS Data Store relational: ') # Only the last values e.g ABC12345 for AGSDataStore_ds_ABC12345
+# https://server.domain.local/wa_server/admin/data/items/enterpriseDatabases
+
+# Information about ArcGIS Data Store servers is maintained using the server URL
+machines_url = f'{server_url}/admin/data/items/enterpriseDatabases/AGSDataStore_ds_{datastore_name}/machines'
+machines_params = {
+    'f': 'json',
+    'token': token
+}
+
+response = requests.get(machines_url, params=machines_params)
+response.raise_for_status()
+machines = response.json()['machines']
+
+# Prints information about ArcGIS Data Store (server distribution)
+for machine in machines:
+    print()
+    print('Machine name:', machine['name'])
+    print('Role:', machine['role'])
+    print()
